50f20f5b7ac358b374a260a8aa46d3d48d291b04
hpraveen <hariharanster@gmail.com>
2019-10-12 19:14:34 -0400

    Modified Card Design

--- clicky-game/src/components/Card/Card.js
+++ clicky-game/src/components/Card/Card.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Card.css";
 
-//this component is only rendering the images to the screen and can be a simple render function it is a
-//stateless component
 const Card = props => (
-  <div className="card img-container hover">
-      <img alt={props.name} src={props.image} id={props.id}
-        onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
   </div>
 );
 
